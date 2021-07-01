3da242acc726ca3006fbab588139c16352214023
Keder <kedernath.mallick.tint022@gmail.com>
2021-07-02 00:40:27 +0530

    tsp

--- dynamicProgramming/travelling_salesman.c
+++ dynamicProgramming/travelling_salesman.c
@@ -1,35 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
-int travellingSalesman(int **Graph,int *visited,int startVertex,int **costMatrix,int vertexNo)
+#include<limits.h>
+int tab[4][4];
+int travellingSalesman(int **Graph,int *set,int position,int startVertex,int vertexNo)
 {
-    if (/* condition */)
-    {
-        /* code */
-    }
-    
+	
+   	printf("Call(%d)\n", position);
+//	set[startVertex]=1;
+	set[position] = 0;
+
+	int  k ,flag = 1,globalMin, localMin = INT_MAX, callForVertex;
+	for (k = 0; k < vertexNo; k++)
+	{
+		if(set[k] != 0 && Graph[position][k] != 0){
+			globalMin = travellingSalesman(Graph,set,k,startVertex,vertexNo);
+			if(globalMin < localMin){
+				localMin = globalMin;
+				callForVertex = k;
+			}
+			flag = 0;
+		}
+	}
+	set[position] = 1;
+	if(flag){
+		tab[position][startVertex]=Graph[position][startVertex];
+		printf("(%d) --> (%d) Final Cost: %d\n", position, startVertex, Graph[position][startVertex]);
+		return Graph[position][startVertex];
+	}
+	tab[position][callForVertex]=localMin+Graph[position][callForVertex];
+	printf("(%d) --> (%d) Cost: %d\n", callForVertex, position, localMin+Graph[position][callForVertex]);
+	return localMin += Graph[position][callForVertex];
 }
 int main(){
-    int **Graph,vertexNo,*visited,**costMatrix,i=0,j=0;
-    scanf("%d",&vertexNo);
-    Graph=(int**)malloc(vertexNo*sizeof(int *));
-    for(i=0;i<vertexNo;i++){
-        Graph[i]=(int *)malloc(vertexNo*sizeof(int));
-    }
-    costMatrix=(int**)malloc(vertexNo*sizeof(int *));
-    for(i=0;i<vertexNo;i++){
-        costMatrix[i]=(int *)malloc(vertexNo*sizeof(int));
-    }
-    visited=(int*)calloc(sizeof(int),vertexNo);
-    for ( i = 0; i < vertexNo; i++)
-    {
-        for (j = 0; j< vertexNo; j++)
-        {
-            scanf("%d",&Graph[i][j]);
-        }
-        
-    }
-    travellingSalesman(Graph,visited,0,costMatrix,vertexNo);
+   int **Graph,vertexNo,i=0,j=0,*set;
+   scanf("%d",&vertexNo);
+   Graph=(int**)malloc(vertexNo*sizeof(int *));
+   for(i=0;i<vertexNo;i++){
+       Graph[i]=(int *)malloc(vertexNo*sizeof(int));
+   }
+   set=(int *)malloc(vertexNo*(sizeof(int )));
+   for(i=0;i<vertexNo;i++){
+       set[i]=1;
+   }
+   for ( i = 0; i < vertexNo; i++)
+   {
+       for (j = 0; j< vertexNo; j++)
+       {
+           scanf("%d",&Graph[i][j]);
+       }
+       
+   }
+   travellingSalesman(Graph,set,0,0,vertexNo);
+   for ( i = 0; i < vertexNo; i++)
+   {
+       for (j = 0; j< vertexNo; j++)
+       {
+           printf("%d ",tab[i][j]);
+       }
+       printf("\n");
+   }
 }
 
-//construct a set for the algo
-    //length of the set shold be calculated.
